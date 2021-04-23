4672bcb475c0183f1bf8a67c35b5818009b70bd8
Daniel Garcia <danigpino@gmail.com>
2021-04-23 09:43:13 +0200

    first version of the tool

---
+++ src/JsonToEnv/JsonToEnv/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+if (args.Length < 2)
+{
+    Console.WriteLine("Usage: json-to-env <input file> <output file>");
+    return;
+}
+
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile(args[0])
+    .Build();
+
+using var writer = File.CreateText(args[1]);
+
+foreach (var setting in configuration.AsEnumerable())
+{
+    var key = setting.Key.Replace(":", "__");
+    var value = setting.Value;
+                
+    if (string.IsNullOrEmpty(value)) continue;
+                
+    writer.WriteLine($"{key}={value}");
+}
+
